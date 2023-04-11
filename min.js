b63e1e6e7468235d9ee31792203652b92ca67e78
jaffa Isaac <jaffaisaac4@gmail.com>
2023-04-11 22:29:38 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,9 @@
+// local scope 
+// the varible is decleared inside the  function or the { ... }
+
+function sum(){
+    let num =40;
+    return 40 *2;
+}
+// console.log(num) // num is not defined
+console.log(sum()) //80
